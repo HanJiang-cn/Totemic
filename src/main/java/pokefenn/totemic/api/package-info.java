47de22983c4745471c630011cacec5fbc24780c0
ljfa <ljfa-ag@users.noreply.github.com>
2024-05-21 20:15:11 +0200

    Version bump
    Change MC version range to exclude 1.20.5+
    
    Change mod release type to beta

--- src/main/java/pokefenn/totemic/api/package-info.java
+++ src/main/java/pokefenn/totemic/api/package-info.java
@@ -5,6 +5,6 @@
  * <p>
  * Feel free to create an issue or pull request on Github to suggest changes or extensions to the API.
  *
- * @version 1.20.4-2.0.0
+ * @version 1.20.4-3.0.0
  */
 package pokefenn.totemic.api;
